a3327830e39a2e08999c4c653c1215d9fac97804
Emma Yan <93347757+emmayly@users.noreply.github.com>
2022-09-12 00:36:46 -0400

    Update GarbageSorting.java

--- app/src/main/java/com/example/greentips/GarbageSorting.java
+++ app/src/main/java/com/example/greentips/GarbageSorting.java
@@ -64,14 +64,17 @@ public class GarbageSorting extends AppCompatActivity {
             mylayout.setOrientation(LinearLayout.VERTICAL);
             TextView t_1 = new TextView(getApplicationContext());
             t_1.setText("Collect food waste and kitchen scraps in a reusable container.");
+            t_1.setPadding(40,0,40,0);
             t_1.setTextColor(Color.WHITE);
             t_1.setTextSize(fontsize);
             TextView t_2 = new TextView(getApplicationContext());
-            t_2.setText("Once full, empty your reusable container into your green bin (provided by the city). Wash the reusable container and place it back in your kitchen.");
+            t_2.setText("Once full, empty your reusable container into your green bin. Wash reusable container and place it back in your kitchen.");
+            t_2.setPadding(40,0,40,0);
             t_2.setTextColor(Color.WHITE);
             t_2.setTextSize(fontsize);
             TextView t_3 = new TextView(getApplicationContext());
             t_3.setText("Bring your green bin to the street curb on days garbage and recycling are collected.");
+            t_3.setPadding(40,0,40,0);
             t_3.setTextColor(Color.WHITE);
             t_3.setTextSize(fontsize);
 
@@ -112,12 +115,13 @@ public class GarbageSorting extends AppCompatActivity {
             mylayout.setOrientation(LinearLayout.VERTICAL);
             TextView t_1 = new TextView(getApplicationContext());
             t_1.setText("Glass products should never be thrown in the garbage, it is often found in the recycling bin in the form of bottles and containers.");
+            t_1.setPadding(40,0,40,0);
             t_1.setTextColor(Color.WHITE);
             t_1.setTextSize(fontsize);
             TextView t_2 = new TextView(getApplicationContext());
-            t_2.setText("Glass can be easily recycled and repurposed. When you recycle your " +
-                            "glass bottles, they are taken to a plant where they are cleaned, " +
+            t_2.setText("Glass can be easily recycled and repurposed. They are taken to a plant where they are cleaned, " +
                             "sorted by color, crushed and melted into new glass products. Glass can always be recycled.");
+            t_2.setPadding(40,0,40,0);
             t_2.setTextColor(Color.WHITE);
             t_2.setTextSize(fontsize);
             AppCompatButton b = new AppCompatButton(getApplicationContext());
@@ -158,7 +162,8 @@ public class GarbageSorting extends AppCompatActivity {
             t_1.setText("The majority of plastic materials are non-biodegradable. After the " +
                     "plastic is recycled, it is purified and melted down. It can be used to " +
                     "create new products, rather than sitting in a landfill and harming the " +
-                    "environment. You can recycle any plastics that have the universal recycling symbol on them.");
+                    "environment. Recycle any plastics that have the universal recycling symbol on them.");
+            t_1.setPadding(40,0,40,0);
             t_1.setTextColor(Color.WHITE);
             t_1.setTextSize(fontsize);
             AppCompatButton b = new AppCompatButton(getApplicationContext());
@@ -196,12 +201,14 @@ public class GarbageSorting extends AppCompatActivity {
             mylayout.setOrientation(LinearLayout.VERTICAL);
             TextView t_1 = new TextView(getApplicationContext());
             t_1.setText("Paper is biodegradable and should be separated in your yellow or blue bag.");
+            t_1.setPadding(40,0,40,0);
             t_1.setTextColor(Color.WHITE);
             t_1.setTextSize(fontsize);
             TextView t_2 = new TextView(getApplicationContext());
             t_2.setText("Keep paper in a blue recycling bin at home so it is easy to transfer over " +
-                    "to a bag on recycling day. Be sure to flatten all cardboard before putting " +
+                    "to a bag on recycling day. Be sure to flatten cardboard before putting " +
                     "it in the recycling bin. It takes up less space and is far more efficient.");
+            t_2.setPadding(40,0,40,0);
             t_2.setTextColor(Color.WHITE);
             t_2.setTextSize(fontsize);
             AppCompatButton b = new AppCompatButton(getApplicationContext());
@@ -241,11 +248,13 @@ public class GarbageSorting extends AppCompatActivity {
             TextView t_1 = new TextView(getApplicationContext());
             t_1.setText("There are many reasons to start recycling your scrap metal. " +
                     "It’s extremely good for the environment, and it can be a great way to " +
-                    "earn a bit of extra money from items that would simply lie around your house otherwise.");
+                    "earn extra money from items that would simply lie around your house otherwise.");
+            t_1.setPadding(40,0,40,0);
             t_1.setTextColor(Color.WHITE);
             t_1.setTextSize(fontsize);
             TextView t_2 = new TextView(getApplicationContext());
-            t_2.setText("You’ll then need to divide up ferrous (magnetic) and non-ferrous (non-magnetic) metals.");
+            t_2.setText("You’ll then need to divide up ferrous (magnetic) and non-ferrous metals.");
+            t_2.setPadding(40,0,40,0);
             t_2.setTextColor(Color.WHITE);
             t_2.setTextSize(fontsize);
             AppCompatButton b = new AppCompatButton(getApplicationContext());
@@ -283,16 +292,19 @@ public class GarbageSorting extends AppCompatActivity {
             mylayout.setGravity(Gravity.CENTER);
             mylayout.setOrientation(LinearLayout.VERTICAL);
             TextView t_1 = new TextView(getApplicationContext());
-            t_1.setText("Place your single-use batteries in a zipper-style plastic bag (see through) beside your recycling box.");
+            t_1.setText("Place your single-use batteries in a zipper-style plastic bag beside your recycling box.");
+            t_1.setPadding(40,0,40,0);
             t_1.setTextColor(Color.WHITE);
             t_1.setTextSize(fontsize);
             TextView t_2 = new TextView(getApplicationContext());
             t_2.setText("9V batteries should be taped to prevent accidental discharge. Batteries " +
                     "exceeding a standard D-Cell battery in size are not acceptable in the program.");
+            t_2.setPadding(40,0,40,0);
             t_2.setTextColor(Color.WHITE);
             t_2.setTextSize(fontsize);
             TextView t_3 = new TextView(getApplicationContext());
             t_3.setText("Please do not place the battery bags inside your blue or grey box.");
+            t_3.setPadding(40,0,40,0);
             t_3.setTextColor(Color.WHITE);
             t_3.setTextSize(fontsize);
             AppCompatButton b = new AppCompatButton(getApplicationContext());
